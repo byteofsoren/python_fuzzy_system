d61a176e38caf5c4023b8b6840d81abf14707092
byteofsoren <r00tr4t@gmail.com>
2020-02-27 15:44:14 +0100

    The fuzzy member calss is done
    Signed-off-by: byteofsoren <r00tr4t@gmail.com>

---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup
+
+
+with open("README.md") as fd:
+    long_description = fd.read()
+
+setup(
+        name='Python_fuzzy_systems',
+        version='0.0.1',
+        description='A fuzzy system for python with fuzzy reasoning and classification.',
+        long_description=long_description,
+        long_description_content_type="text/markdown",
+        py_modules=["python_fuzzy_system"],
+        package_dir={'':'src'},
+        classifiers=[
+            "Programming Language :: Python :: 3",
+            "Programming Language :: Python :: 3.6",
+            "Programming Language :: Python :: 3.7",
+            "Programming Language :: Python :: 3.8",
+            "License :: OSI Approved :: MIT License",
+            "Operating System :: OS independent",
+            ]
+        )
